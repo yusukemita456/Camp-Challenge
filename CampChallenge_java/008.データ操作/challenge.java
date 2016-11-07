6cd59a1a5f24cc2cfc82260c1299cf9521c55d4e
yusukemita <rt2310126rt@gmail.com>
2016-11-07 17:12:53 +0900

    JAVAデータ操作課題３

---
+++ CampChallenge_java/008.データ操作/challenge.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ * @author user2
+ */
+@WebServlet("/challenge")
+public class challenge extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    
+    
+    //３．クッキーに現在時刻を記録し、次にアクセスした際に、前回記録した日時を表示してください。
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        
+            
+            
+            
+       
+       //初回の登録      
+       Date time = new Date();//現在の日時をインスタンス
+       Cookie c = new Cookie("LastLogin", time.toString());//クッキーのインスタンス
+       response.addCookie(c);//クッキーの追加
+        
+       request.setCharacterEncoding("UTF-8");//コードの選択
+        String name = request.getParameter("name");//名前の受取
+        String gender = request.getParameter("gender");//性別
+        String hoby = request.getParameter("hoby");//趣味
+        
+        Cookie cs[] = request.getCookies();
+        int i = 0;
+        if(cs != null){
+            for(i = 0;i <cs.length;i++){
+            if (cs[i].getName().equals("LastLogin")) {
+                out.print("最後のログインは、"+cs[i].getValue());
+                break;
+         }
+        }
+         }
+        String msg = name + gender + hoby;//情報をまとめる
+        
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet challenge</title>");            
+            out.println("</head>");
+            out.println("<body>");
+             out.println("<p>" + msg +"</p>");//メッセージの表示
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ CampChallenge_java/008.データ操作/challenge.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : challenge
+    Created on : 2016/11/07, 13:18:06
+    Author     : user2
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form action="challenge" method="POST">
+            名前:<input type="text" name="name"><br>   
+            性別:
+            男:<input type="radio" name="gender" value="男">
+            女:<input type="radio" name="gender" value="女"><br>
+            趣味:<input type="textarea" name="hoby"><br>
+            <input type="submit" value="送信" >
+            
+            
+        </form>
+    </body>
+</html>
