2bb0393202dee4b491a7a2a1d3b098f8e6f1a83f
yusukemita <rt2310126rt@gmail.com>
2016-11-07 17:13:19 +0900

    JAVAデータ操作課題1,2

---
+++ CampChallenge_java/008.データ操作/challenge1.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ *
+ * @author user2
+ */
+@WebServlet("/challenge1")
+public class challenge1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    
+         //課題１．以下の入力フィールドを持ったHTMLを、Javaで処理する想定で記述してください。
+        //・名前（テキストボックス）、性別（ラジオボタン）、趣味（複数行テキストボックス）
+        //課題２．以下の機能を実装してください。
+        //１で作成したHTMLの入力データを取得し、画面に表示する
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        
+       
+     
+        request.setCharacterEncoding("UTF-8"); //コードの選択
+        String name = request.getParameter("name");//名前の受取
+        String gender = request.getParameter("gender");//性別の受取
+        String hoby = request.getParameter("hoby");//趣味の受取
+        
+        
+        String msg = name + gender + hoby;//全ての表示をまとめる
+        
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet challenge</title>");            
+            out.println("</head>");
+            out.println("<body>");
+             out.println("<p>" + msg +"</p>");//ページに表示させる
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
